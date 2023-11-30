f8b6e42e20029084eb05f201ac0c4ae7cd57d757
HP <reynaldi.adidarma@binus.ac.id>
2023-11-30 22:36:10 +0700

    modelToTeam method

--- Hackathon-TeamManagement_GSLC-OOP/src/main/TeamRepository.java
+++ Hackathon-TeamManagement_GSLC-OOP/src/main/TeamRepository.java
@@ -3,6 +3,8 @@ package main;
 import java.util.ArrayList;
 
 public class TeamRepository implements Repository{
+	
+	ArrayList<Team> teamList;
 
 	public ArrayList<Model> stringToObject(ArrayList<String> dataTeam) {
 		ArrayList<Model> result = new ArrayList<Model>();
@@ -13,6 +15,18 @@ public class TeamRepository implements Repository{
 		}
 		
 		return result;
+	}
+	
+	public ArrayList<Team> modelToTeam(ArrayList<String> dataTeam){
+		
+		ArrayList<Model> modelUser = this.stringToObject(dataTeam);
+		ArrayList<Team> teamList = new ArrayList<Team>();
+		
+		for (Model model : modelUser) {
+			teamList.add((Team)model);
+		}
+		
+		return teamList;
 	}
 
 	public Model find(String col, String condition, Boolean join, String tableJoin, Connection conn) {
